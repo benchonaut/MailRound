a409026b5bc4f057eacf97f3b5be9de0cf4fdc1e
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-02-14 14:09:37 +0100

    2023-02-14-13.09

--- mailround/controller/round_trip.py
+++ mailround/controller/round_trip.py
@@ -200,7 +200,8 @@ If MailRound works it will be deleted""")
 
         if mail_round_uuid is None:
             self.log.info("Found Mail without UUID ")
-            self.log.info(json.dumps(email_body.get_all("",[]), sort_keys=True, indent=4))
+            for dumpheader in('From','To', 'CC', 'BCC','X-Mail-Round');
+            self.log.info(json.dumps(email_body.get_all(dumpheader), sort_keys=True, indent=4))
             return False
         else:
             self.log.info("Found Mail with UUID "+ str(mail_round_uuid)+ "| MY LOCAL UID IS:"+self.uuid.hex)
