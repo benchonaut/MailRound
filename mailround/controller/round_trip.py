0dc5c984b43a2d773930ab704645e75d30f1056f
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-02-14 14:57:24 +0100

    2023-02-14-13.57

--- mailround/controller/round_trip.py
+++ mailround/controller/round_trip.py
@@ -162,12 +162,12 @@ Kind Regards
             else:
                 msg.add_header(newheader, str(self.uuid.hex)+'@X-Mail-Round.lan')
         max_time = settings.MAX_MAIL_RECEIVE_TIME
-        date_time = datetime.now()
+        date_time = datetime.datetime.now()
         msgstr+'X-Mail-Round='+str(self.uuid.hex)
         msgstr=msgstr+tmpnewline+'X-Mail-Generated-At='+date_time.strftime("%m/%d/%Y, %H:%M:%S")
 
         #if datetime.datetime.now() > start_timestamp + max_time
-        msg_timeout=datetime.fromtimestamp(start_timestamp + max_time)
+        msg_timeout=datetime.datetime.fromtimestamp(start_timestamp + max_time)
         msgstr=msgstr+tmpnewline+'X-Mail-Timeout-At='+msg_timeout.strftime("%m/%d/%Y, %H:%M:%S")
         msg.set_content(msgstr)
         #debug2
